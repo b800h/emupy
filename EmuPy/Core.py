7c763b2680f49b977b7703326886fa1d8c068525
unknown <BCollier@.gb.computacenter.co.uk>
2014-08-10 15:13:11 +0100

    modified:   EmuPy/Core.py

--- EmuPy/Core.py
+++ EmuPy/Core.py
@@ -10,16 +10,24 @@ VERSION = 0.1
 
 class clock:
     def __init__ (self, speed):
-        self.lasttime = time()
+        self.lasttime = time.clock() #Use time.time() on Linux
         self.cycle = 0
+        self.speed = speed
         self.normalinterval = 1 / (speed * 1000000)
+        print "Clock running at " + str(self.speed) + "MHz"
+        print "Internal clock interval: " + str(self.normalinterval) + " seconds."
     def tick (self):
         self.cycle = self.cycle + 1
+        if self.cycle == 4770000:
+            self.cycle = 0
+            print "Second"
+            
     def run(self):
-        self.thistime = time()
+        self.thistime = time.clock() #Use time.time() on linux
         self.interval = self.thistime - self.lasttime
         if self.interval > self.normalinterval:
-            self.tick     
+            self.lasttime = self.thistime
+            self.tick()
 
 if __name__ == '__main__':
     pass
@@ -29,6 +37,7 @@ print "EmuPy v" + str(VERSION) + " initialising..."
 clock1 = clock(4.77) # Instantiate clock running at 4.77 MHz
 
 print "EmuPy running. Press CTRL-C to end emulation."
+breakflag = 0
 
-while 1:
-    clock1.run
+while breakflag == 0:
+    clock1.run()
