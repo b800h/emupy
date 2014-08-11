d79288a27402c979ee2ea57dec2b2f953de39386
Ben Collier <bmcollier@gmail.com>
2014-08-11 15:29:17 +0100

    emupy/components/__init__.pyc 	emupy/components/chip_4164.pyc

--- emupy/internal.py
+++ emupy/internal.py
@@ -13,11 +13,11 @@ VERSION = 0.1
 
 # Global System Considerations - Farm this out to System Config Python File
 
-Clock = [0];
-Bus = {
+bus = {
        'A0': 0, 'A1': 0, 'A2': 0, 'A3': 0, 'A4': 0, 'A5': 0, 'A6': 0, 'A7': 0,
        'A8': 0, 'A9': 0, 'A10': 0, 'A11': 0, 'A12': 0, 'A13': 0, 'A14': 0, 'A15': 0,
-       'D0': 0, 'D1': 0, 'D2': 0, 'D3': 0, 'D4': 0, 'D5': 0, 'D6': 0, 'D7': 0
+       'D0': 0, 'D1': 0, 'D2': 0, 'D3': 0, 'D4': 0, 'D5': 0, 'D6': 0, 'D7': 0,
+       'CLK': 0
 };
 
 # Clock object - Add this to clock.py
